d14093f1fbd2ac3934e5dcd06521c3527fcefed5
Levi Zimmerman <l.zimmerman@wearetriple.com>
2024-06-04 11:11:05 +0200

    feat(ios): adds live activity widget

---
+++ ios/TimerWidget/TimerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TimerWidgetBundle.swift
+//  TimerWidget
+//
+//  Created by Levi Zimmerman on 29/05/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TimerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TimerWidgetLiveActivity()
+    }
+}
---
+++ ios/TimerWidget/TimerWidgetLiveActivity.swift
@@ -0,0 +1,85 @@
+//
+//  TimerWidgetLiveActivity.swift
+//  TimerWidget
+//
+//  Created by Levi Zimmerman on 29/05/2024.
+//
+
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct TimerWidgetAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic stateful properties about your activity go here!
+        var emoji: String
+    }
+
+    // Fixed non-changing properties about your activity go here!
+    var name: String
+}
+
+struct TimerWidgetLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: TimerWidgetAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            VStack {
+                Text("Hello \(context.state.emoji)")
+            }
+            .activityBackgroundTint(Color.cyan)
+            .activitySystemActionForegroundColor(Color.black)
+
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Text("Leading")
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text("Trailing")
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("Bottom \(context.state.emoji)")
+                    // more content
+                }
+            } compactLeading: {
+                Text("B")
+            } compactTrailing: {
+                Text("T \(context.state.emoji)")
+            } minimal: {
+                Text(context.state.emoji)
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.red)
+        }
+    }
+}
+
+extension TimerWidgetAttributes {
+    fileprivate static var preview: TimerWidgetAttributes {
+        TimerWidgetAttributes(name: "World")
+    }
+}
+
+extension TimerWidgetAttributes.ContentState {
+    fileprivate static var smiley: TimerWidgetAttributes.ContentState {
+        TimerWidgetAttributes.ContentState(emoji: "😀")
+     }
+     
+     fileprivate static var starEyes: TimerWidgetAttributes.ContentState {
+         TimerWidgetAttributes.ContentState(emoji: "🤩")
+     }
+  
+    fileprivate static var eggPlant: TimerWidgetAttributes.ContentState {
+        TimerWidgetAttributes.ContentState(emoji: "🍆")
+    }
+}
+
+#Preview("Notification", as: .content, using: TimerWidgetAttributes.preview) {
+   TimerWidgetLiveActivity()
+} contentStates: {
+    TimerWidgetAttributes.ContentState.eggPlant
+    TimerWidgetAttributes.ContentState.smiley
+    TimerWidgetAttributes.ContentState.starEyes
+}
