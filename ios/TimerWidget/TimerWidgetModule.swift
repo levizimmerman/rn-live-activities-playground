3fc9ee1da517e3d5730618cc6e82927f3bad4ad8
Levi Zimmerman <l.zimmerman@wearetriple.com>
2024-06-05 10:44:16 +0200

    feat(ios): connect activity kit to module

--- ios/TimerWidget/TimerWidgetModule.swift
+++ ios/TimerWidget/TimerWidgetModule.swift
@@ -6,17 +6,37 @@
 //
 
 import Foundation
+import ActivityKit
 
 @objc(TimerWidgetModule)
 class TimerWidgetModule: NSObject {
   
+  private func areActivitiesEnabled() -> Bool {
+    return ActivityAuthorizationInfo().areActivitiesEnabled
+  }
+  
   @objc
   func startLiveActivity() -> Void {
+    if (!areActivitiesEnabled()) {
+      return
+    }
     
+    let activityAttributes = TimerWidgetAttributes()
+    let contentState = TimerWidgetAttributes.ContentState(startedAt: Date())
+    let activityContent = ActivityContent(state: contentState, staleDate: nil)
+    do {
+      try Activity.request(attributes: activityAttributes, content: activityContent)
+    } catch {
+      // Handle errors
+    }
   }
   
   @objc
   func stopLiveActivity() -> Void {
-    
+    Task {
+      for activity in Activity<TimerWidgetAttributes>.activities {
+        await activity.end(nil, dismissalPolicy: .immediate)
+      }
+    }
   }
 }
